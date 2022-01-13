e7f7edec14e37f2d574bb6baeeb1380574cfde31
RoukiaMI <mahadismailroukia@gmail.com>
2022-01-13 11:34:41 +0100

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsProduitComponent } from './pages/details-produit/details-produit.component';
+import { HeaderComponent } from './features/header/header.component'
+import { FooterComponent } from './features/footer/footer.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'home', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'detailsproduit', component: DetailsProduitComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './features/header/header.component';
+import { FooterComponent } from './features/footer/footer.component';
+import { DetailsProduitComponent } from './pages/details-produit/details-produit.component';
+import { ProduitsService } from './core/services/produits.service';
+import { ProduitComponent } from './pages/produit/produit.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailsProduitComponent,
+    ProduitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProduitsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
