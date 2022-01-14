ca1a97fee2a11d5ea47e0b94aed037ef09c84881
Alexandre <77535743+Alex91700@users.noreply.github.com>
2022-01-14 09:57:15 +0100

    Style navBar + ajout derniere page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,20 @@ import { HomeComponent } from './pages/home/home.component';
 import { DetailsProduitComponent } from './pages/details-produit/details-produit.component';
 import { HeaderComponent } from './features/header/header.component'
 import { FooterComponent } from './features/footer/footer.component'
+import { ChiffreAffaireComponent } from './features/chiffre-affaire/chiffre-affaire.component';
+import { DonneesHistoriqueComponent } from './pages/donnees-historique/donnees-historique.component';
 
 const routes: Routes = [
- { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'header', component: HeaderComponent },
   { path: 'footer', component: FooterComponent },
-  { path: 'detailsproduit', component: DetailsProduitComponent }];
+  { path: 'detailsproduit', component: DetailsProduitComponent },
+  { path: 'donneesHistorique', component: DonneesHistoriqueComponent},
+  { path: 'chiffreAffaire', component: ChiffreAffaireComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/features/chiffre-affaire/chiffre-affaire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chiffre-affaire',
+  templateUrl: './chiffre-affaire.component.html',
+  styleUrls: ['./chiffre-affaire.component.css']
+})
+export class ChiffreAffaireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
